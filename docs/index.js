95217e1639ec8ddd8447c1e5e0cb4e4c8c846f88
purvik6062 <purvikpanchal6336@gmail.com>
2024-01-31 16:35:33 +0530

    package file

--- docs/index.js
+++ docs/index.js
@@ -3,13 +3,20 @@ import { render } from 'react-dom';
 import { fakeRequest } from './request';
 import { timeSlotValidator } from './validators';
 import DayTimeScheduler from '../src';
-
-const selectedDays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+// import theme from './theme';
+import styled from 'styled-components';
 
 function App() {
   const [isScheduling, setIsScheduling] = useState(false);
   const [isScheduled, setIsScheduled] = useState(false);
   const [scheduleErr, setScheduleErr] = useState('');
+  const allowedDates = ['2024-02-01', '2024-02-02', '2024-02-03', '2024-02-05', '2024-02-06', '2024-02-08', '2024-02-14'];
+  const allowedTimeRanges = [
+    [10, 0, 12, 0],
+    [14, 0, 20, 0]
+  ];
+
+  const selectedDays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
   const handleScheduled = date => {
     setIsScheduling(true);
@@ -29,14 +36,61 @@ function App() {
       });
   };
 
-  return (
-    <DayTimeScheduler
-      selectedDays={selectedDays}
-      timeSlotSizeMinutes={15}
-      timeSlotValidator={timeSlotValidator}
-      onConfirm={handleScheduled}
-    />
+  function getDayName(dateString) {
+    const dayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    const date = new Date(dateString);
+    const dayIndex = date.getDay();
+    return dayNames[dayIndex];
+  }
 
+  function timeSlotValidator2(slotTime, allowedDates, allowedTimeRanges) {
+    const formattedDate = slotTime.toISOString().split('T')[0];
+    const isDateValid = allowedDates.includes(formattedDate);
+
+    if (!isDateValid) {
+      return false;  // Return false for slots on non-allowed dates
+    }
+
+    const dayName = getDayName(formattedDate);
+    const isDayValid = selectedDays.includes(dayName);
+
+    const isTimeValid = allowedTimeRanges.some(([startHour, startMinute, endHour, endMinute]) => {
+      const startTime = new Date(
+        slotTime.getFullYear(),
+        slotTime.getMonth(),
+        slotTime.getDate(),
+        startHour,
+        startMinute,
+        0
+      );
+      const endTime = new Date(
+        slotTime.getFullYear(),
+        slotTime.getMonth(),
+        slotTime.getDate(),
+        endHour,
+        endMinute,
+        0
+      );
+      return slotTime.getTime() >= startTime.getTime() && slotTime.getTime() <= endTime.getTime();
+    });
+
+    return isDayValid && isTimeValid;
+  }
+
+  return (
+    <>
+      <div style={{ margin: "0 auto", marginTop: "2rem", boxShadow: "0px 0px 4px -1px black", width: "fit-content", }}>
+        <DayTimeScheduler
+          onConfirm={handleScheduled}
+          selectedDays={selectedDays}
+          timeSlotSizeMinutes={15}
+          isLoading={isScheduling}
+          isDone={isScheduled}
+          // timeSlotValidator={timeSlotValidator}
+          timeSlotValidator={(slotTime) => timeSlotValidator2(slotTime, allowedDates, allowedTimeRanges)}
+        />
+      </div>
+    </>
   );
 }
 
--- docs/validators.js
+++ docs/validators.js
@@ -6,16 +6,58 @@
  *
  * @return {Boolan} If the time slot is valid or not.
  */
-export function timeSlotValidator(slotTime) {
-  const eveningTime = new Date(
-    slotTime.getFullYear(),
-    slotTime.getMonth(),
-    slotTime.getDate(),
-    14,
-    0,
-    0
-  );
+// export function timeSlotValidator(slotTime) {
+//   const eveningTime = new Date(
+//     slotTime.getFullYear(),
+//     slotTime.getMonth(),
+//     slotTime.getDate(),
+//     14,
+//     0,
+//     0
+//   );
 
-  const isValid = slotTime.getTime() > eveningTime.getTime();
-  return isValid;
+//   const isValid = slotTime.getTime() > eveningTime.getTime();
+//   return isValid;
+// }
+
+function getDayName(dateString) {
+  const dayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  const date = new Date(dateString);
+  const dayIndex = date.getDay();
+  return dayNames[dayIndex];
 }
+
+export function timeSlotValidator(slotTime, allowedDates, allowedTimeRanges) {
+  const formattedDate = slotTime.toISOString().split('T')[0];
+  const isDateValid = allowedDates.includes(formattedDate);
+
+  if (!isDateValid) {
+    return false;  // Return false for slots on non-allowed dates
+  }
+
+  const dayName = getDayName(formattedDate);
+  const isDayValid = selectedDays.includes(dayName);
+
+  const isTimeValid = allowedTimeRanges.some(([startHour, startMinute, endHour, endMinute]) => {
+    const startTime = new Date(
+      slotTime.getFullYear(),
+      slotTime.getMonth(),
+      slotTime.getDate(),
+      startHour,
+      startMinute,
+      0
+    );
+    const endTime = new Date(
+      slotTime.getFullYear(),
+      slotTime.getMonth(),
+      slotTime.getDate(),
+      endHour,
+      endMinute,
+      0
+    );
+    return slotTime.getTime() >= startTime.getTime() && slotTime.getTime() <= endTime.getTime();
+  });
+
+  return isDayValid && isTimeValid;
+}
+
